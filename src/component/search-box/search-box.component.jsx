1eed1a4ceba1a49f74c220af711446f95e6bb681
kapilll <50981562+kapilll@users.noreply.github.com>
2020-03-09 04:58:54 +0530

    Made a small mistake now It's all good.

--- src/component/search-box/search-box.component.jsx
+++ src/component/search-box/search-box.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './search-box.styles.css';
 
-export const SearchBox=({placeholder,handleChange})=>(
+export const SearchBox=({placeholder,onSearchChange})=>(
     <input className="search" type="search"
     placeholder={placeholder}
-    onChange={handleChange} />
+    onChange={onSearchChange} />
 )
