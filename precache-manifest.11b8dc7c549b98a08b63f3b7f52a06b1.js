d3b2fe6900f59eae135ef83f1acf8b441971b93f
kapil <kapilstarksaiyan@gmail.com>
2020-03-09 04:53:32 +0530

    Updates

---
+++ precache-manifest.11b8dc7c549b98a08b63f3b7f52a06b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3b957d272c22fd3b653b4c391d1e8a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "060eead2a8e705cc220b",
+    "url": "/monsters-rolodex/static/css/main.9849e1aa.chunk.css"
+  },
+  {
+    "revision": "7136eb73ea9c81d0121c",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.cc8af93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060eead2a8e705cc220b",
+    "url": "/monsters-rolodex/static/js/main.d9ba7ccb.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
